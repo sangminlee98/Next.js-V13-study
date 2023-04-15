e0549af49827e318a84e8dc6d8b85345c7a28a8c
sangminlee98 <tkdals0975@naver.com>
2023-04-16 04:52:11 +0900

    feat : [note] API Route 사용해보기

---
+++ note/src/app/api/products/route.ts
@@ -0,0 +1,7 @@
+import { getProducts } from "@/services/products";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const products = await getProducts();
+  return NextResponse.json({ products });
+}
