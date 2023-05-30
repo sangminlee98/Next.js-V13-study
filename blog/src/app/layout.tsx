feb7a6051c175c8d3b2205d469fc1511750f6824
sangminlee98 <tkdals0975@naver.com>
2023-05-31 02:45:26 +0900

    feat : [blog] Header 컴포넌트 추가 및 CSS

--- blog/src/app/layout.tsx
+++ blog/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import "./globals.css";
 import { Open_Sans } from "next/font/google";
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={sans.className}>{children}</body>
+      <body className={sans.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ blog/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center px-12 h-16 max-w-screen-xl my-0 mx-auto">
+      <h1 className="text-2xl font-bold">Sangmin&apos;s Blog</h1>
+      <ul className="flex gap-4">
+        <Link href={"/"}>home</Link>
+        <Link href={"/about"}>about</Link>
+        <Link href={"/posts"}>posts</Link>
+        <Link href={"/contact"}>contact</Link>
+      </ul>
+    </div>
+  );
+}
