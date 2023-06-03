7aeadf1f8d079c5c767c0f94f33db4ce6a075046
sangminlee98 <tkdals0975@naver.com>
2023-06-03 19:41:32 +0900

    feat : [blog] 메인페이지 포스트 추가

--- blog/src/components/Header.tsx
+++ blog/src/components/Header.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Header() {
   return (
     <div className="flex justify-between items-center px-12 h-16 max-w-screen-xl my-0 mx-auto">
-      <h1 className="text-2xl font-bold">Sangmin&apos;s Blog</h1>
+      <h1 className="text-2xl font-bold">Sam&apos;s Blog</h1>
       <ul className="flex gap-4">
         <Link href={"/"}>home</Link>
         <Link href={"/about"}>about</Link>
